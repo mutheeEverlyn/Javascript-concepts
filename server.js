93d4142c690ab024fa32f35dff20b00d4c762739
Everlyn Muthoni <muthee.14952@students.kyu.ac.ke>
2024-08-02 01:49:25 -0500

    added concepts

---
+++ server.js
@@ -0,0 +1,16 @@
+//object
+let person={
+    name:"Everlyn",
+    age:22,
+    education:{
+     year:4,
+     course:"software engineering",
+     location:{
+        county:"kirinyaga",
+        town:"kutus"
+     }
+    }
+}
+
+console.log(person.education.location.town)
+
