68f03ac030ad0ced4775dcd6522068de31a68366
Everlyn Muthoni <muthee.14952@students.kyu.ac.ke>
2024-08-03 03:51:36 -0500

    added file

---
+++ operators.js
@@ -0,0 +1,15 @@
+// arithmetic operators
+let a=10;
+let b=5;
+
+console.log(`the value of ${a} + ${b} is ${a + b}`); //addition
+console.log(`the value of ${a} - ${b} is ${a - b}`); //subtraction
+console.log(`the value of ${a} * ${b} is ${a * b}`); //multiplication
+console.log(`the value of ${a} / ${b} is ${a / b}`); //division
+console.log(`the value of ${a} % ${b} is ${a % b}`); //modulus
+console.log(`the value of ${a} ** ${b} is ${a ** b}`); //exponential
+console.log(`the value of a++ is ${a++}`);  //post increment
+console.log(`the value of ++a is ${++a}`);  //pre increment
+console.log(`the value of b-- is ${b--}`);  //post decrement
+console.log(`the value of --b is ${--b}`);  //pre decrement
+
