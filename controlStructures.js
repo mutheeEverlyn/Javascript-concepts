4aa260ec5f8f25746014a0da550eef6ba3cd90c6
Everlyn Muthoni <muthee.14952@students.kyu.ac.ke>
2024-08-05 14:32:55 +0300

    conditional statements

---
+++ controlStructures.js
@@ -0,0 +1,10 @@
+let num1=10;
+let num2=20;
+
+//if statement
+//if(condition){
+// code to be executed if the condition is true
+// }
+if (num2>num1){
+    console.log(`${num2} is greater than ${num1}`)
+}
