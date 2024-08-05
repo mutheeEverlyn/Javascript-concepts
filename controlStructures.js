5531d7302a9a1256992f00ec5f097985901afa86
Everlyn Muthoni <muthee.14952@students.kyu.ac.ke>
2024-08-05 14:34:31 +0300

    conditional statements

--- controlStructures.js
+++ controlStructures.js
@@ -24,3 +24,22 @@ else{
     console.log(`${num1} is less than ${num2}`)
 }
 
+//if...elseif...else statement
+//if( first condition){
+// code to be executed if the first condition is true
+// }
+//else if(second condition){
+//code to be executed if the second condition is true
+//}
+// else{
+//  code to be executed if all the conditions are false
+// }
+if (num1>num2){
+    console.log(`${num1} is greater than ${num2}`)
+}
+else if(num2>num1){
+    console.log(`${num1} is less than ${num2}`)
+}else{
+    console.log(`${num1} is equal to ${num2}`)
+}
+
