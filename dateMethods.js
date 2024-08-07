bb1e0586ec684d3cd8acc033b6b523651260d425
Everlyn Muthoni <muthee.14952@students.kyu.ac.ke>
2024-08-07 13:23:36 -0600

    changes

---
+++ dateMethods.js
@@ -0,0 +1,14 @@
+let date = new Date()
+console.log(date)
+console.log(date.getDate())  //1-31
+console.log(date.getDay())//0-6  ,0-Sunday...
+console.log(date.getFullYear())  //2024
+console.log(date.getHours())  //0-23
+console.log(date.getMilliseconds())  //0-999
+console.log(date.getMinutes())  //0-59
+console.log(date.getMonth())  //0-11 ,0-jan.....
+console.log(date.getSeconds())  //0-59
+console.log(date.getTime())  //timestamp
+console.log(date.toDateString())
+console.log(date.toLocaleDateString())
+console.log(date.toTimeString())
