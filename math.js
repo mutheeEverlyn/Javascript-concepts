75c50048f0d01ee0c58feb32884da3a4103735d0
Everlyn Muthoni <muthee.14952@students.kyu.ac.ke>
2024-08-07 13:25:09 -0600

    changes

--- math.js
+++ math.js
@@ -4,3 +4,33 @@ console.log(Math.LN2)
 console.log(Math.LOG10E)
 console.log(Math.LOG2E)
 console.log(Math.PI)
+
+
+//Math methods
+//abs:returns the absolute value of the given number
+console.log(Math.abs(-6)) //6
+
+//max:returns the maximum number
+console.log(Math.max(50,70,34)) //70
+
+//min:returns the minimum value number
+console.log(Math.min(6,7,9))  //6
+
+//ceil:rounds up the number to the nearest whole number
+console.log(Math.ceil(8.2)) //9
+
+//floor:rounds down the number to the nearest whole number
+console.log(Math.floor(6.9)) //6
+
+//round:round the number to the nearest whole number
+console.log(Math.round(4.3))  //4
+console.log(Math.round(5.6))  //6
+
+//random:gives a randon number
+console.log(Math.random())
+
+//sqrt:give the square root of the number
+console.log(Math.sqrt(25))  //5
+
+//pow:finds the square root
+console.log(Math.pow(2,3))  //8
