b1d88e2f0b15c0d4abc24cc5b0fbeb0ffdb159b9
Everlyn Muthoni <muthee.14952@students.kyu.ac.ke>
2024-08-08 11:35:44 -0600

    added concepts

---
+++ functions.js
@@ -0,0 +1,15 @@
+//1.normal function
+
+//function declaration
+function sayHello(){
+    console.log('hello there')
+}
+//function call
+sayHello()
+
+//normal function with a parameter
+function hello(name){
+    console.log(`hello there ${name}`)
+}
+hello('evah');
+
