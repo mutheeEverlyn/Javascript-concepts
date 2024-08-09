3ff5771f9615cf2037f03404973aa6ec2c0e356f
Everlyn Muthoni <muthee.14952@students.kyu.ac.ke>
2024-08-09 10:45:06 -0600

    added functions concepts

--- functions.js
+++ functions.js
@@ -39,3 +39,14 @@ return num1+num2;
 }
 console.log(sum(1,4))
 
+//4.immediately invoked function expression(IIFE) :does not required a function call
+(function name() {
+    console.log("good morning");
+})();
+
+(() => { 
+    console.log("good evening my friend");
+})();
+
+
+
