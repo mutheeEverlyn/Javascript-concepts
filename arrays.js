b47bedf230f2640ff5ca72136103bcaafc827fe6
Everlyn Muthoni <muthee.14952@students.kyu.ac.ke>
2024-08-11 13:30:49 -0600

    changes

---
+++ arrays.js
@@ -0,0 +1,6 @@
+//arrays: used to store values which are indexed and are separated by a comma
+//index: 0,1,2,3.....
+
+//create an array
+//1.using array literal
+let snacks=['chocolate','yoghurt','capcakes','juice']
