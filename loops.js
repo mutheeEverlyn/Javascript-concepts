053f65fa4a08b5fbdff84205a1c1b2d95b04552f
Everlyn Muthoni <muthee.14952@students.kyu.ac.ke>
2024-08-12 20:14:42 +0300

    loops concepts

---
+++ loops.js
@@ -0,0 +1,31 @@
+let animals=['monkey','lion','antelop','gazzel','cheetah']
+let countries=[{
+    name:'Kenya',
+    capital_city:'Nairobi'
+},
+{
+    name:'Uganda',
+    capital_city:'Kampala'
+},
+{
+    name:'Tanzania',
+    capital_city:'Mogadishu'
+},
+{
+    name:'Burundi',
+    capital_city:'Bujumbura'
+},
+]
+let person={
+    name:'evah',
+    age:22,
+    weight:62
+}
+//forEach loop: used to loop over an array or an array of objects
+animals.forEach((animal)=>{
+    console.log(animal)
+})
+countries.forEach((country)=>{
+    console.log(country)
+})
+
