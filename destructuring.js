c086b53111cf075e0fba12ff6cc7409ee4b43951
Everlyn Muthoni <muthee.14952@students.kyu.ac.ke>
2024-08-17 12:48:53 -0600

    added js concepts

---
+++ destructuring.js
@@ -0,0 +1,12 @@
+//destructuring : used to extract data from nested objects and arrays
+let people=
+    {
+        name:'paul',
+        age:60,
+        address:{
+            country:'Kenya',
+            city:'Nairobi'
+        }
+    }
+
+
