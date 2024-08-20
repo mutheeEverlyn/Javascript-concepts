4d45530360580292189b8a7acfe367102d3abfc7
Everlyn Muthoni <muthee.14952@students.kyu.ac.ke>
2024-08-20 19:46:31 +0300

    js concepts added

--- objects.js
+++ objects.js
@@ -13,3 +13,28 @@ food.ingredients=['rice','meat','ginger','garlic','pilau masala'];
 food.ratings=5;
 
 
+
+//loopimg over items in an object
+let fruits=[
+    {
+        name:'mango',
+        ratings:3
+    },
+    {
+        name:'pawpaw',
+        ratings:4
+    },
+    {
+        name:'banana',
+        ratings:3
+    },
+    {
+        name:'apple',
+        ratings:5
+    },
+    {
+        name:'pineapple',
+        ratings:4
+    }
+]
+
