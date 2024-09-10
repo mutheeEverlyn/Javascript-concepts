66e52f98dcac607de0d7aeaef94a9f84764369d0
Everlyn Muthoni <muthee.14952@students.kyu.ac.ke>
2024-09-10 01:04:51 -0500

    added concepts

--- concepts.js
+++ concepts.js
@@ -3,3 +3,8 @@ console.log("hello there");
 console.log("today we are learning")
 console.log("synchronous code")
 
+//asynchronous code : code that does not execute in an orderly manner
+console.log("this is ")
+setTimeout(()=>{console.log("an example of an")},5000)
+console.log("asynchronous code")
+
