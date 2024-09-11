ab28502d01ca95ace05c98aeaabe0944e56c5030
Everlyn Muthoni <muthee.14952@students.kyu.ac.ke>
2024-09-11 08:25:29 -0500

    changes

--- concepts.js
+++ concepts.js
@@ -8,3 +8,15 @@ console.log("this is ")
 setTimeout(()=>{console.log("an example of an")},5000)
 console.log("asynchronous code")
 
+//error handling
+try{
+    let num="";
+    num=Number(num)
+    if(isNaN(num)) throw("this is not a number")
+    else if(num=="") throw("you have not entered a number")
+        else{
+    console.log(num)
+    }
+    }catch(error){
+        console.log(error)
+    }
