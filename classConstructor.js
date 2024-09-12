5642950d0c9fd4fc9cfc3b2814933d8fb134674d
Everlyn Muthoni <muthee.14952@students.kyu.ac.ke>
2024-09-12 14:17:42 -0500

    classes in js

---
+++ classConstructor.js
@@ -0,0 +1,16 @@
+//class: it is a blueprint that is used to create various objects
+// class without a constructor
+class Car{
+    start(){
+        console.log("the car has started");
+    }
+
+    stop(){
+        console.log("the car ha sstopped")
+    }
+}
+
+const car1=new Car()
+car1.start()
+car1.stop()
+
