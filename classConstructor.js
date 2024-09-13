2ea33eddc03b107e69e01f774e900137c073c17d
Everlyn Muthoni <muthee.14952@students.kyu.ac.ke>
2024-09-13 03:05:49 -0500

    added concepts

--- classConstructor.js
+++ classConstructor.js
@@ -14,3 +14,20 @@ const car1=new Car()
 car1.start()
 car1.stop()
 
+//class with a constructor :special method of a class
+class Student{
+    constructor(name,age,gpa){
+    this.name=name;
+    this.age=age;
+    this.gpa=gpa;
+    }
+
+    details(){
+        console.log(`hello ${this.name} you are ${this.age} years old and you have a gpa of ${this.gpa}`)
+    }
+}
+
+const student1=new Student("emily",22,3.2);
+console.log(student1)
+student1.details()
+
