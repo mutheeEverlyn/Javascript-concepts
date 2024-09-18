14ec027dd14e28f5adb65edf5209cc7c4205cfe4
Everlyn Muthoni <muthee.14952@students.kyu.ac.ke>
2024-09-18 18:49:04 +0300

    new file

---
+++ DOM/pattern.js
@@ -0,0 +1,36 @@
+// let rows=window.prompt("enter the rows:");
+// let columns=window.prompt("enter the columns");
+// let style=window.prompt("enter the symbol to use:");
+
+//rectangle or square
+// for(let i=1;i<=rows;i++){
+//     for(let j=1;j<=columns;j++){
+//         document.getElementById("pattern").innerHTML +=style;
+//     }
+//     document.getElementById("pattern").innerHTML +="<br/>";
+// }
+
+// document.getElementById("btn").onclick=function(){
+//     let rows;
+//     document.getElementById("rows").innerHTML +=rows;
+//     let columns;
+//     document.getElementById("columns").innerHTML +=columns;
+//     let style;
+//     document.getElementById("style").innerHTML +=style; 
+
+// for(let i=1;i<=rows;i++){
+//     for(let j=1;j<=columns;j++){
+//         document.getElementById("rectangle").innerHTML +=style;
+//     }
+//     document.getElementById("rectangle").innerHTML +="<br/>";
+// }
+
+// }
+
+// creating a number seven pattern
+for(let i=1;i<=4;i++){
+    for(let j=1;j<=4;j++){   
+  console.log("*")
+    }
+    console.log("\b")
+}
